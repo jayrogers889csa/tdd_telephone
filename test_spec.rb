db59cb1e8980e8cb001d26bc5ee22fcde9939476
jayrogers889csa <jayrogers889@gmail.com>
2014-03-11 10:40:24 -0500

    initial test

---
+++ test_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+describe Pizza do
+
+  context "Existence Test" do
+    it "creates a Pizza object" do
+      pizza = Pizza.new()
+      pizza.should be_an_instance_of Pizza
+    end
+  end
+
+end
