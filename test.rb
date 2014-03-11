516f52ef2f59b3ac9d045c22140701cadd13bbcf
Tara Roys <tlroys@gmail.com>
2014-03-11 15:35:50 -0500

    Pizza should have many toppings!

--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
 class Pizza
-  attr_accessor :name, :time_baked, :description
+  attr_accessor :name, :time_baked, :description, :toppings
 
   def initialize(args)
     @name = args[:name]
     @description = args[:description]
     @time_baked = 0
+    @toppings = [0, 1]
 
   end
 end
