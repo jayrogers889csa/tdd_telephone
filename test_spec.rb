67fb7c0158a58d917ec65ad11dbc8abdc18bc0ca
jayrogers889csa <jayrogers889@gmail.com>
2014-03-11 11:14:52 -0500

    fixed test

--- test_spec.rb
+++ test_spec.rb
@@ -3,13 +3,6 @@ require 'rspec'
 
 describe Pizza do
 
-  context "Existence Test" do
-    it "creates a Pizza object" do
-      pizza = Pizza.new()
-      pizza.should be_an_instance_of Pizza
-    end
-  end
-
   context "Name test" do
     it "must have a name" do
       expect { Pizza.new }.to raise_error(ArgumentError)
